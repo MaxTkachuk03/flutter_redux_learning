a8bb0d20cb7b4943c46cd3543365df6b9daec4f0
MaxTkachuk03 <max200325622@gmail.com>
2024-10-16 23:13:11 +0300

    store provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_redux_learning/flutter_redux_app.dart';
+import 'package:flutter_redux_learning/redux/redux.dart';
 
 void main() {
-  runApp(const FlutterReduxApp());
+  final store = getStore();
+  runApp(StoreProvider(
+    store: store,
+    child: const FlutterReduxApp(),
+  ));
 }
